0371f499f8374f6bd7b2a7193039f5d65a8ce37d
chungw51993 <chungw51993@gmail.com>
2017-02-21 11:23:17 -0600

    updating when typing in text

--- server/request-handler.js
+++ server/request-handler.js
@@ -20,8 +20,9 @@ var defaultCorsHeaders = {
 
 var output = {
   results: [{
-    "username": "Jin",
-    "text": "hello"
+    'username': 'Jono',
+    'text': 'Do my bidding!',
+    'objectId': Math.random() * 10000
   }]
 };
 
@@ -44,14 +45,18 @@ var requestHandler = function(request, response) {
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
   var statusCode = 404;
   // The outgoing status.
-  if (request.url === '/classes/messages') {
+  if (request.url.split("?")[0] === '/classes/messages') {
     if (request.method === 'GET') {
       statusCode = 200;
+      console.log('---------------------------->results', output.results);
+
     } else if (request.method === 'POST') {
       statusCode = 201;
       request.on('data', function(datum) {
         var postData = '';
         postData += datum;
+        postData = (parser(postData));
+        postData = JSON.stringify(postData);
         postData = JSON.parse(postData);
         output.results.push(postData);
       });
@@ -65,6 +70,22 @@ var requestHandler = function(request, response) {
       headers["access-control-max-age"] = '86400'; // 24 hours
       headers["access-control-allow-headers"] = "x-parse-application-id, x-parse-rest-api-key, Content-Type, Accept";
     } 
+  }
+
+  //i surpisingly had to make my own parser function. i gota be missing something
+  var parser = function(string) {
+    //input: username=Jin&text=hello&roomname=lobby
+    //output: { username: 'Jono', text: 'Do my bidding!' }
+    var prettyObj = {};
+    var stringArr = string.split("&");
+    var username = stringArr[0].split("=")
+    var text = stringArr[1].split("=")
+    prettyObj[username[0]] = username[1].split("+").join(" ");
+    prettyObj[text[0]] = text[1].split("+").join(" ");
+    prettyObj['objectId'] = Math.random() * 10000;
+
+    return prettyObj
+
   }
 
   // See the note below about CORS headers.
